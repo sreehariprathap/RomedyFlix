12e8148fe1aa87c3ebc89dcd9ec910e519ac9855
Sreehari Prathap <sreehariprathap1996@gmail.com>
2023-06-25 22:59:32 +0530

    minor changes

--- src/components/Buttons/ScrollButton.tsx
+++ src/components/Buttons/ScrollButton.tsx
@@ -7,7 +7,7 @@ const ScrollButton: React.FC<any> = ({ action, isPrevious }) => {
       <button
         className={`absolute ${
           isPrevious ? "right-10 top-50 mr-[-3rem] bg-gradient-to-l from-black to-transparent" : " left-0 top-50 bg-gradient-to-r from-black to-transparent"
-        } z-20   w-10 h-[450px]  flex justify-center items-center hover:backdrop-blur-sm hover:bg-black duration-200 ease-linear`}
+        } z-20   w-10 h-[450px]  flex justify-center items-center hover:backdrop-blur-sm hover:bg-dark duration-200 ease-linear`}
         onClick={action}
       >
         {isPrevious ? (
--- src/components/Cards/ProgrammeCard.tsx
+++ src/components/Cards/ProgrammeCard.tsx
@@ -3,14 +3,13 @@ import { useState } from "react"
 import RoundActionButton from "../Buttons/RoundActionButton"
 import { getImageSrcWithAPIKey } from "../../features/landing/api/getImage"
 
-const ProgrammeCard = ({
-  description,
-  programme,
-}) => {
+const ProgrammeCard: React.FC<any> = ({ programme }) => {
   const [isExpanded, setIsExpanded] = useState(false)
   const toggleExpansion = () => {
     setIsExpanded(!isExpanded)
   }
+
+  console.log(programme)
 
   const expandingCardVariants = {
     hidden: { opacity: 0, x: 10 },
@@ -24,7 +23,7 @@ const ProgrammeCard = ({
       whileHover={{ opacity: 1, scale: 1.1 }}
       transition={{ duration: 0.3 }}
       onMouseEnter={toggleExpansion}
-      onMouseLeave={()=>setIsExpanded(false)}
+      onMouseLeave={() => setIsExpanded(false)}
       variants={expandingCardVariants}
     >
       <div className="w-[13rem] h-[25rem] flex items-end flex-col">
@@ -34,9 +33,10 @@ const ProgrammeCard = ({
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5 }}
-            className="p-5 text-white bg-dark-secondary text-sm w-full"
+            className="py-2 px-2 text-white bg-dark-secondary text-sm w-full"
           >
-            <div className="action-bar p-1 flex gap-3">
+            <h2 className="pb-1 line-clamp-1">{programme.original_title || programme.title}</h2>
+            <div className="action-bar p-1 flex gap-3 pt-2">
               <RoundActionButton
                 icon={"play"}
                 styles={"bg-slate-100 hover:bg-slate-200 text-dark"}
@@ -48,7 +48,6 @@ const ProgrammeCard = ({
                 hoverText={"Episode and info"}
               />
             </div>
-            <p className="card-text">{description}</p>
           </motion.div>
         )}
       </div>
