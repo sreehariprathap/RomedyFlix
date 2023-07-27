ae94c216cf11bff802c188e8587a9f2bae27cf72
Sreehari Prathap <sreehariprathap1996@gmail.com>
2023-07-27 21:09:06 +0530

    programme details modal

--- src/components/Modals/ProgramDetailsModal.tsx
+++ src/components/Modals/ProgramDetailsModal.tsx
@@ -1,6 +1,8 @@
 import React, { useRef } from "react"
 import { getImageSrcWithAPIKey } from "../../features/landing/api/getImage"
 import RoundActionButton from "../Buttons/RoundActionButton"
+import { XMarkIcon } from "@heroicons/react/24/solid"
+import { Languages, MovieGenre } from "../../config/Constants"
 
 interface ModalProps {
   id: string
@@ -33,42 +35,80 @@ const ProgramDetailsModal: React.FC<ModalProps> = ({
 
   return (
     <>
-    <div onClick={showModal}>{children}</div>
-    <dialog
-      id={id}
-      className="modal modal-bottom sm:modal-middle"
-      ref={modalRef}
-    >
-      <form
-        method="dialog"
-        className="w-[45rem] overflow-hidden rounded-2xl shadow-xl bg-dark relative"
+      <div onClick={showModal}>{children}</div>
+      <dialog
+        id={id}
+        className="modal modal-bottom sm:modal-middle"
+        ref={modalRef}
       >
-        <div>
-          <img
-            src={getImageSrcWithAPIKey(programData.backdrop_path)}
-            alt=""
-            className="w-full h-[25rem]"
-          />
-          <div className="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <RoundActionButton
-              icon={"play"}
-              styles={
-                "bg-slate-100 hover:bg-slate-200 text-dark w-[3rem] h-[3rem]"
-              }
+        <form
+          method="dialog"
+          className="w-[55rem] overflow-hidden rounded-2xl shadow-xl bg-dark relative "
+        >
+          <div>
+            <div className="absolute w-full bg-transparent hover:bg-gradient-to-b from-black to-transparent ease-in-out transition duration-300 h-10">
+              <button
+                className="absolute right-0 px-2 py-1"
+                onClick={closeModal}
+              >
+                <XMarkIcon className="w-5 h-5" />
+              </button>
+            </div>
+            <img
+              src={getImageSrcWithAPIKey(programData.backdrop_path)}
+              alt=""
+              className="w-full h-[25rem]"
             />
+            <div className="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <RoundActionButton
+                icon={"play"}
+                styles={
+                  "bg-slate-100 hover:bg-slate-200 text-dark w-[3rem] h-[3rem]"
+                }
+              />
+            </div>
           </div>
-        </div>
-        <p className="py-4">
-          Press ESC key or click the button below to close
-        </p>
-        <div className="modal-action">
-          <button className="btn" onClick={closeModal}>
-            Close
-          </button>
-        </div>
-      </form>
-    </dialog>
-  </>
+          <div className="py-4 px-2">
+            <div className="flex justify-between items-center">
+              <h2 className="text-4xl font-bold">
+                {programData.original_title}
+              </h2>
+              <div className="flex gap-3">
+                <div className="bg-primary text-white rounded-md p-1 text-sm font-medium">
+                  {programData.vote_average}
+                </div>
+                <div className="border bg-transparent flex justify-center items-center rounded-md px-1">
+                  <p className="text-[0.5rem]"> U/A 13+</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="flex gap-1 pt-1 items-center">
+                <div className="text-white p-1 text-sm font-medium">
+                  {
+                    Languages[
+                      programData.original_language as keyof typeof Languages
+                    ]
+                  }
+                </div>
+                <div>|</div>
+                {programData.genre_ids.map((id: number) => (
+                  <div className="text-white p-1 text-sm font-medium">
+                    {MovieGenre[id]},
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="py-2">
+              <p className="text-sm font-thin">{programData.overview}</p>
+            </div>
+          </div>
+        </form>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
   )
 }
 
